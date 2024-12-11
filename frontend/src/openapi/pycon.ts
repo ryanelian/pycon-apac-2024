e74fc4bb4c117be03712d5683130e6ecda6001ab
Ryan Elian <8892187+ryanelian@users.noreply.github.com>
2024-12-11 11:23:23 +0900

    fix

--- frontend/src/openapi/pycon.ts
+++ frontend/src/openapi/pycon.ts
@@ -31,43 +31,6 @@ export interface paths {
         /** Get Users V1 */
         get: operations["get_users_v1_v1_users_get"];
         put?: never;
-        /** Create User V1 */
-        post: operations["create_user_v1_v1_users_post"];
-        delete?: never;
-        options?: never;
-        head?: never;
-        patch?: never;
-        trace?: never;
-    };
-    "/v1/users/{id}": {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
-        };
-        /** Get User Details V1 */
-        get: operations["get_user_details_v1_v1_users__id__get"];
-        put?: never;
-        post?: never;
-        /** Delete User V1 */
-        delete: operations["delete_user_v1_v1_users__id__delete"];
-        options?: never;
-        head?: never;
-        /** Update User V1 */
-        patch: operations["update_user_v1_v1_users__id__patch"];
-        trace?: never;
-    };
-    "/v2/users": {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
-        };
-        /** Get Users V2 */
-        get: operations["get_users_v2_v2_users_get"];
-        put?: never;
         post?: never;
         delete?: never;
         options?: never;
@@ -79,71 +42,34 @@ export interface paths {
 export type webhooks = Record<string, never>;
 export interface components {
     schemas: {
-        /** HTTPValidationError */
-        HTTPValidationError: {
-            /** Detail */
-            detail?: components["schemas"]["ValidationError"][];
+        /** PasscodeNotRequired */
+        PasscodeNotRequired: {
+            /**
+             * Require Passcode
+             * @default false
+             * @constant
+             * @enum {boolean}
+             */
+            require_passcode: false;
         };
-        /** NotFound404 */
-        NotFound404: {
-            /** Detail */
-            detail: string;
-        };
-        /** V1UserCreateAndUpdateRequestModel */
-        V1UserCreateAndUpdateRequestModel: {
+        /** PasscodeRequired */
+        PasscodeRequired: {
             /**
-             * Username
-             * @description Username should be between 3 and 64 characters
+             * Require Passcode
+             * @default true
+             * @constant
+             * @enum {boolean}
              */
-            username: string;
-            /**
-             * Family Name
-             * @description Family name must not exceed 255 characters
-             */
-            family_name: string;
-            /**
-             * Given Name
-             * @description Given name must not exceed 255 characters
-             */
-            given_name: string;
+            require_passcode: true;
+            /** Passcode Length */
+            passcode_length: number;
         };
         /** V1UserResponseModel */
         V1UserResponseModel: {
             /** Id */
             id: string;
-            /** Username */
-            username: string;
-            /** Family Name */
-            family_name: string;
-            /** Given Name */
-            given_name: string;
-        };
-        /** V2UserListResponseModel */
-        V2UserListResponseModel: {
-            /** Items */
-            items: components["schemas"]["V2UserResponseModel"][];
-            /** Cursor */
-            cursor: string | null;
-        };
-        /** V2UserResponseModel */
-        V2UserResponseModel: {
-            /** Id */
-            id: string;
-            /** Username */
-            username: string;
-            /** Family Name */
-            family_name: string;
-            /** Given Name */
-            given_name: string;
-        };
-        /** ValidationError */
-        ValidationError: {
-            /** Location */
-            loc: (string | number)[];
-            /** Message */
-            msg: string;
-            /** Error Type */
-            type: string;
+            /** Passcode */
+            passcode: components["schemas"]["PasscodeNotRequired"] | components["schemas"]["PasscodeRequired"];
         };
     };
     responses: never;
@@ -190,195 +116,6 @@ export interface operations {
                 };
                 content: {
                     "application/json": components["schemas"]["V1UserResponseModel"][];
-                };
-            };
-        };
-    };
-    create_user_v1_v1_users_post: {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
-        };
-        requestBody: {
-            content: {
-                "application/json": components["schemas"]["V1UserCreateAndUpdateRequestModel"];
-            };
-        };
-        responses: {
-            /** @description Successful Response */
-            201: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": string;
-                };
-            };
-            /** @description Validation Error */
-            422: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": components["schemas"]["HTTPValidationError"];
-                };
-            };
-        };
-    };
-    get_user_details_v1_v1_users__id__get: {
-        parameters: {
-            query?: never;
-            header?: never;
-            path: {
-                id: string;
-            };
-            cookie?: never;
-        };
-        requestBody?: never;
-        responses: {
-            /** @description Successful Response */
-            200: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": components["schemas"]["V1UserResponseModel"];
-                };
-            };
-            /** @description Resource not found */
-            400: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": components["schemas"]["NotFound404"];
-                };
-            };
-            /** @description Validation Error */
-            422: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": components["schemas"]["HTTPValidationError"];
-                };
-            };
-        };
-    };
-    delete_user_v1_v1_users__id__delete: {
-        parameters: {
-            query?: never;
-            header?: never;
-            path: {
-                id: string;
-            };
-            cookie?: never;
-        };
-        requestBody?: never;
-        responses: {
-            /** @description Successful Response */
-            200: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": string;
-                };
-            };
-            /** @description Resource not found */
-            400: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": components["schemas"]["NotFound404"];
-                };
-            };
-            /** @description Validation Error */
-            422: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": components["schemas"]["HTTPValidationError"];
-                };
-            };
-        };
-    };
-    update_user_v1_v1_users__id__patch: {
-        parameters: {
-            query?: never;
-            header?: never;
-            path: {
-                id: string;
-            };
-            cookie?: never;
-        };
-        requestBody: {
-            content: {
-                "application/json": components["schemas"]["V1UserCreateAndUpdateRequestModel"];
-            };
-        };
-        responses: {
-            /** @description Successful Response */
-            200: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": string;
-                };
-            };
-            /** @description Resource not found */
-            400: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": components["schemas"]["NotFound404"];
-                };
-            };
-            /** @description Validation Error */
-            422: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": components["schemas"]["HTTPValidationError"];
-                };
-            };
-        };
-    };
-    get_users_v2_v2_users_get: {
-        parameters: {
-            query?: {
-                /** @description The last user ID from the previous page */
-                cursor?: string | null;
-            };
-            header?: never;
-            path?: never;
-            cookie?: never;
-        };
-        requestBody?: never;
-        responses: {
-            /** @description Successful Response */
-            200: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": components["schemas"]["V2UserListResponseModel"];
-                };
-            };
-            /** @description Validation Error */
-            422: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": components["schemas"]["HTTPValidationError"];
                 };
             };
         };
